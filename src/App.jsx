6ce46f1fe9f485997f3c7f09616d5f59ac791ad9
vatiza <rakibur.mac@gmail.com>
2024-09-27 12:00:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@nextui-org/react";
+
+function App() {
+  return (
+    <>
+
+      <Button color="primary">Button</Button>
+    </>
+  );
+}
+
+export default App;
